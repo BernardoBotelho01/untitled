a859aaa65cae7cf961b655374dd06360446b27de
Bernardo_Botelho <82350234+BernardoBotelho01@users.noreply.github.com>
2024-05-23 20:01:45 -0300

    Update MainQuiz.java

--- MainQuiz.java
+++ MainQuiz.java
@@ -5,7 +5,7 @@ public class MainQuiz {
     System.out.println();
     System.out.println("-------Faculdade Unifan-------");
     System.out.println("Aluno: Bernardo Jose Oliveira Botelho");
-    System.out.println("Professor: Breno Pimenta");
+    System.out.println("Professor: Brenno Pimenta");
     System.out.println("Tema das Perguntas: Conhecimentos básicos sobre Java!");
     System.out.println();
     // Criação de um objeto LogicaQuiz e início do quiz
